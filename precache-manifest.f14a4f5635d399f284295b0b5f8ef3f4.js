3b9e1fb4e7d3d28a79c383e17fc3a0d08bde7962
koftov <ahronlu@gmail.com>
2020-04-09 23:54:13 +0300

    Updates

---
+++ precache-manifest.f14a4f5635d399f284295b0b5f8ef3f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a32c34c4b6b6785980f9e816bdf3806",
+    "url": "/react-tailwind-pixabay-gallery/index.html"
+  },
+  {
+    "revision": "0647ee96832e0a41eb5a",
+    "url": "/react-tailwind-pixabay-gallery/static/css/main.119390ab.chunk.css"
+  },
+  {
+    "revision": "9a322e8e8008560c36ad",
+    "url": "/react-tailwind-pixabay-gallery/static/js/2.0c3d36ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tailwind-pixabay-gallery/static/js/2.0c3d36ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0647ee96832e0a41eb5a",
+    "url": "/react-tailwind-pixabay-gallery/static/js/main.f8778b1d.chunk.js"
+  },
+  {
+    "revision": "bff7b00fad30ed46f228",
+    "url": "/react-tailwind-pixabay-gallery/static/js/runtime-main.f711efc2.js"
+  }
+]);
